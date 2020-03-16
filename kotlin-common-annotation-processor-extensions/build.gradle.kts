f07824f103d61ba6e9e232e36ee9c60272681a96
Leo Lin <leolin310148@gmail.com>
2020-03-17 01:08:30 +0800

    publish kotlin-common-annotation-processor-extensions module

--- kotlin-common-annotation-processor-extensions/build.gradle.kts
+++ kotlin-common-annotation-processor-extensions/build.gradle.kts
@@ -2,8 +2,5 @@ dependencies {
   implementation("com.squareup:kotlinpoet:1.5.0")
 }
 
-tasks.filter { it.group == "publishing" }.forEach {
-  it.enabled = false
-}
 tasks.getByName("printCoverage").enabled = false
 tasks.getByName("jacocoTestReport").enabled = false
