ed8c9db56baf605e2c00debf374b5229c407a408
Leo Lin <leolin310148@gmail.com>
2020-03-21 21:55:11 +0800

    add EnableJpaRepositories to ZygardeAutoConfigure

--- kotlin-common-spring-boot-zygarde/src/main/kotlin/puni/zygarde/autoconfigure/ZygardeAutoConfigure.kt
+++ kotlin-common-spring-boot-zygarde/src/main/kotlin/puni/zygarde/autoconfigure/ZygardeAutoConfigure.kt
@@ -2,7 +2,9 @@ package puni.zygarde.autoconfigure
 
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @Configuration
+@EnableJpaRepositories("puni.zygarde.data.dao")
 @ComponentScan("puni.zygarde")
 class ZygardeAutoConfigure
